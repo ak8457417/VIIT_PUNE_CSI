e2c4a3afe4fe556b9b6c2779f6f067e02aa09eab
pranavbafna586 <pranavbafna586@gmail.com>
2025-02-21 14:13:46 +0530

    Balaji Logo changes

--- src/Components/Sponsor/Sponsor.jsx
+++ src/Components/Sponsor/Sponsor.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import './Sponser.css'
-import verve from '../../assets/verve.png'
+import React from "react";
+import "./Sponser.css";
+import verve from "../../assets/verve.png";
 
 const Sponsor = () => {
-    return (
-        <div className="sponsor">
-            <div className="sponsor-title">
-                <h1>Our Sponsors</h1>
-            </div>
-            <div className="cards-sponsor">
-                <div className="sponsor-card">
-                    <img src={"https://play-lh.googleusercontent.com/Sn4FCvXPL21HgxKO_-Oj0_ldYA8YH2muN6enGKpBylzvYpCa_cZTqbU6VEXYRtFGwg"} alt={'unstop'} />
-                </div>
-                    <div className="sponsor-card">
-                        <img src={verve} alt={'verve'} />
-                    </div>
-                    <div className="sponsor-card">
-                        <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/BalajiWafersLogo.svg/1200px-BalajiWafersLogo.svg.png"} alt={'balaji'} />
-                    </div>
-            </div>
+  return (
+    <div className="sponsor">
+      <div className="sponsor-title">
+        <h1>Our Sponsors</h1>
+      </div>
+      <div className="cards-sponsor">
+        <div className="sponsor-card">
+          <img
+            src={
+              "https://d8it4huxumps7.cloudfront.net/uploads/images/unstop/branding-guidelines/logos/blue/Unstop-Logo-Blue.svg"
+            }
+            alt={"unstop"}
+          />
         </div>
-    );
+        <div className="sponsor-card">
+          <img src={verve} alt={"verve"} />
+        </div>
+        <div className="sponsor-card">
+          <img src={"/download.png"} alt={"balaji"} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sponsor;
